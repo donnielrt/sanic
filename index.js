254f3f4adb2ef51e365dc0cffb5a3c57fea2b9e0
Donniel Thomas <donniel@mobify.com>
2015-11-02 15:57:46 -0800

    Yeeeeeah

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var _ = require('lodash');
+var https = require('https');
 
 var port = process.env.PORT ? process.env.PORT : 3000;
+var repeat = function(str, count) {
+    var output = str;
+    for (var ctr = 0; ctr < count - 1; ctr++) {
+        output += str;
+    }
+
+    return output;
+};
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -14,21 +23,31 @@ app.get('/', function(req, res) {
     res.send('Ready!');
 });
 
-app.post('/', function(req, res) {
-    var excitement = req.body.text || Math.floor(Math.random() * 10, 10);
-    var repeat = function(str, count) {
-        var output = str;
-        for (var ctr = 0; ctr < count - 1; ctr++) {
-            output += str;
-        }
+app.post('/test', function() {
+    console.log('Received data');
+});
 
-        return output;
-    };
+app.post('/', function(req, res) {
+    var excitement = Math.min(req.body.text, 100) || Math.floor(Math.random() * 10, 10);
 
     var howFast = repeat('a', excitement);
-    var tmpl = _.template('Gotta go f<%= howFast %>st!');
+    var sanicSaysWut = _.template('Gotta go f<%= howFast %>st!')({ howFast: howFast });
 
-    res.send(tmpl({ howFast: howFast }));
+    // For Slack
+    var slackData = { text: sanicSaysWut };
+    var slackOptions = {
+        hostname: 'hooks.slack.com',
+        port: 443,
+        path: '/services/T031VND2D/B0DMPEDL2/hCwClN3buIm9dR6kwhztfXRh',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    };
+    var slackReq = https.request(slackOptions);
+
+    slackReq.write(JSON.stringify(slackData));
+    slackReq.end();
+
+    res.send(sanicSaysWut);
 });
 
 http.listen(port, function() {
